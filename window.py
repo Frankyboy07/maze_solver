efe732acee2dadba6f7fdd74a4a564209b8a5fe0
Frankyboy07 <skylinefranc@gmail.com>
2025-01-23 23:12:53 -0600

    Window Class

---
+++ window.py
@@ -0,0 +1,50 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.canvas = Canvas(self.__root, width=self.width, height=self.height)
+        self.canvas.pack()
+        self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+    
+    def close(self):
+        self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2)
+
+
+def main():
+    win = Window(800,600)
+    win.draw_line(Line(Point(100,200),Point(150,250)), 'black')
+    win.draw_line(Line(Point(100,200),Point(250,250)), 'red')
+    win.wait_for_close()
+
+if __name__ == '__main__':
+    main()
